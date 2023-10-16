3ce448ab6ac56395c381abd467c8537570dc7d4c
Mian <mzhong@ethz.ch>
2023-10-16 18:01:38 +0200

    clean columns and build dictionary

--- data-raw/data-processing.R
+++ data-raw/data-processing.R
@@ -13,22 +13,44 @@ gdho_full <- gdho_raw |>
   dplyr::rename_all(tolower) #TODO: country names need more cleaning, remove ","
 
 ## Modify data types
-### integer: id, year, year_founded, year_closed, countries_of_operation_count, staff, natl, intl
-### factor: type, international_or_national, sector, religious_or_secular, red_cross_code_of_conduct_signatory, chs_member, interaction_member, icva_member
+### integer:
+gdho_full <- gdho_full |>
+  dplyr::mutate(across(c("id", "year", "year_founded", "year_closed",
+                         "countries_of_operation_count", "staff", "natl", "intl"),
+                       as.integer)) |>
+### factor:
+  dplyr::mutate(across(c("type", "international_or_national", "sector",
+                         "religious_or_secular", "red_cross_code_of_conduct_signatory",
+                         "chs_member", "interaction_member", "icva_member"),
+                       as.factor))
 ### char -> dbl: %_intl
-
+  dplyr::mutate(across(c()))
 ### TODO:staff_, natl_, intl_imputed columns look super weird
-### TODO: Money columns
+### TODO: Money columns remove dollar sign (described in dictionary file) and convert to double
+gdho_full <- gdho_full |>
+  dplyr::mutate(across(c("ope_approx_usd", "ope/staff", "ope_inflation_adjusted",
+                         "humexp_approx_usd", "humexp_inflation_adjusted"), ~ gsub("[$,]", "", .x))) |>
+  dplyr::mutate(across(c("ope_approx_usd", "ope/staff", "ope_inflation_adjusted",
+                         "humexp_approx_usd", "humexp_inflation_adjusted"), as.double))
+
+
 glimpse(gdho_full)
 
 
-gdho <- gdho_full[1:33]
+gdho <- gdho_full[1:33] # a shorter version that does not include all country columns
 
 ## Read and write dictionary ---------------------------------------------------
-dictionary <- read_excel("./data-raw/gdho_read_me.xlsx", skip = 2)
+original_dict <- read_excel("./data-raw/gdho_read_me.xlsx", skip = 2)
+dictionary <- tibble(directory = "data",
+       file_name = "gdho_full.rda",
+       variable_name = c(colnames(gdho_full)[1:33], "countries"),
+       variable_type =  c(sapply(gdho_full, typeof)[1:33], "integer"),
+       description = original_dict$`Content description`)
+
 
 ## ---------
-usethis::use_data(data-processing, overwrite = TRUE)
+usethis::use_data(gdho, overwrite = TRUE)
+usethis::use_data(gdho_full, overwrite = TRUE)
 
 library(leaflet)
 library(leaflet.extras)
